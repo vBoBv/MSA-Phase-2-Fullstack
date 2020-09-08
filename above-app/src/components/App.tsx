fcd26800de5800a567eb3d01e024e879299c7ae6
vBoBv <vannponhvath@gmail.com>
2020-09-08 17:11:59 +1200

    Setup MaterialUI and Added bgImg to LandingPage

--- above-app/src/components/App.tsx
+++ above-app/src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from './Header/Header';
 import LandingPage from './LandingPage/LandingPage';
 
 const App = () => {
 	return (
 		<div>
+			<CssBaseline />
 			<Header />
 			<LandingPage />
 		</div>
--- above-app/src/components/LandingPage/LandingPage.tsx
+++ above-app/src/components/LandingPage/LandingPage.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import backgroundImg from '../../assets/space.jpg';
+
+const useStyles = makeStyles((theme) => ({
+	backgroundImage: {
+		backgroundImage: `url(${backgroundImg})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'top',
+		position: 'relative',
+		height: '100vh'
+	}
+}));
 
 const LandingPage = () => {
-	return <div>LandingPage</div>;
+	const classes = useStyles();
+	const theme = useTheme();
+
+	return (
+		<Grid container className={classes.backgroundImage}>
+			<div></div>
+			<div></div>
+		</Grid>
+	);
 };
 
 export default LandingPage;
