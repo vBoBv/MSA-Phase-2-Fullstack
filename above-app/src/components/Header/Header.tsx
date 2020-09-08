afbd77d608723cf56fcfedcb6e9035c737c5803a
vBoBv <vannponhvath@gmail.com>
2020-09-08 20:43:21 +1200

    implemented wowjs

--- above-app/src/components/Header/Header.tsx
+++ above-app/src/components/Header/Header.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Grid } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import WOW from 'wowjs';
+
+import logo from '../../assets/logo.png';
+
+const useStyles = makeStyles((theme) => ({
+	navBarContainer: {
+		position: 'absolute',
+		color: '#FFFFFF',
+		paddingTop: '2rem'
+	},
+	logo: {
+		width: '80px',
+		height: '80px'
+	}
+}));
 
 const Header = () => {
-	return <div>Header</div>;
+	useEffect(() => {
+		new WOW.WOW({
+			live: false
+		}).init();
+	}, []);
+
+	const classes = useStyles();
+
+	return (
+		<Grid container justify='center' className={classes.navBarContainer}>
+			<Grid item>
+				<img
+					className={`${classes.logo} wow flipInX`}
+					src={logo}
+					alt='Logo'
+					data-wow-duration='infinite'
+					data-wow-delay='.5s'
+				/>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Header;
