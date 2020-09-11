407f05689b2ea59cff53a668af41c701b1e0631c
vBoBv <vannponhvath@gmail.com>
2020-09-11 19:57:10 +1200

    added react router

--- above-app/src/components/App.tsx
+++ above-app/src/components/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from './Header/Header';
 import LandingPage from './LandingPage/LandingPage';
 import Particle from './Particles/Particles';
+import AcquiredObjects from './AcquiredObjects/AcquiredObjects';
+
+import './App.css';
 
 const App = () => {
 	return (
 		<div>
 			<CssBaseline />
-			<Header />
-			<LandingPage />
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route exact path='/' component={LandingPage} />
+					<Route path='/items' component={AcquiredObjects} />
+				</Switch>
+			</BrowserRouter>
 			<Particle />
 		</div>
 	);
