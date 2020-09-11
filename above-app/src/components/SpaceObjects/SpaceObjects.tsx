2f5172bdf396eacfb9dbef5a521fe9e389caecc9
vBoBv <vannponhvath@gmail.com>
2020-09-11 22:17:18 +1200

    configured SpaceObjects to use CardGrid

--- above-app/src/components/SpaceObjects/SpaceObjects.tsx
+++ above-app/src/components/SpaceObjects/SpaceObjects.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardGrid from '../CardGrid/CardGrid';
+import { Grid, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { IItem } from '../../common/Interfaces';
+
+import backgroundImage from '../../assets/spaceObjects.jpg';
+import Api from '../api/Api';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme) => ({
+	spaceObjectsContainer: {
+		backgroundImage: `url(${backgroundImage})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'top',
+		position: 'relative',
+		paddingTop: '10rem',
+		height: '100%',
+		color: 'white'
+	},
+	title: {
+		marginBottom: '3rem'
+	},
+	addNew: {
+		marginBottom: '3rem'
+	}
+}));
 
 const SpaceObjects = () => {
-	return <div style={{ color: 'red' }}>Space Objects</div>;
+	const classes = useStyles();
+	const [spaceObjects, setSpaceObjects] = useState<IItem[]>([]);
+
+	useEffect(() => {
+		Api.Items.list().then((response) => {
+			setSpaceObjects(response);
+		});
+	}, []);
+
+	return (
+		<div className={classes.spaceObjectsContainer}>
+			<Grid container justify='center' alignItems='center' direction='column'>
+				<Grid item className={classes.title}>
+					<Typography variant='h4'>Identified / Unidentified Flying Objects</Typography>
+				</Grid>
+				<Grid item className={classes.addNew}>
+					<Button variant='contained' color='primary' size='small' startIcon={<AddIcon />}>
+						Add New Object
+					</Button>
+				</Grid>
+			</Grid>
+
+			<CardGrid spaceObjects={spaceObjects} />
+		</div>
+	);
 };
 
 export default SpaceObjects;
