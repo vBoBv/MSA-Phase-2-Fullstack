2102e583f075534c890f5a8a9ea4d1e7cecfc81f
vBoBv <vannponhvath@gmail.com>
2020-09-11 19:44:09 +1200

    refactored NavigationTabs

--- above-app/src/components/NavigationTabs/NavigationTabs.tsx
+++ above-app/src/components/NavigationTabs/NavigationTabs.tsx
@@ -1,78 +1,45 @@
 import React from 'react';
-import { makeStyles, withStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import { Link, NavLink } from 'react-router-dom';
 
-interface StyledTabsProps {
-	value: number;
-	onChange: (event: React.ChangeEvent<{}>, newValue: number) => void;
-}
-
-const StyledTabs = withStyles({
-	indicator: {
-		display: 'flex',
-		justifyContent: 'center',
-		backgroundColor: 'transparent',
-		'& > span': {
-			maxWidth: 50,
-			width: '100%',
-			backgroundColor: '#26bdb8'
-		}
-	}
-})((props: StyledTabsProps) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
-
-interface StyledTabProps {
-	label: string;
-}
-
-const StyledTab = withStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			textTransform: 'none',
-			color: '#fff',
-			fontWeight: theme.typography.fontWeightRegular,
-			fontSize: theme.typography.pxToRem(15),
-			marginRight: theme.spacing(1),
-			'&:focus': {
-				opacity: 1
-			}
-		}
-	})
-)((props: StyledTabProps) => <Tab disableRipple {...props} />);
-
-const useStyles = makeStyles((theme: Theme) => ({
-	root: {
-		flexGrow: 1
-	},
-	padding: {
-		paddingBottom: theme.spacing(3)
-	},
+const useStyles = makeStyles((theme) => ({
 	tabsContainer: {
-		backgroundColor: 'transparent'
+		width: 700
+	},
+	tab: {
+		fontSize: '1rem'
+	},
+	link: {
+		color: 'inherit',
+		textDecoration: 'none'
 	}
 }));
 
 const NavigationTabs = () => {
 	const classes = useStyles();
-	const [value, setValue] = React.useState(0);
-
-	const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-		setValue(newValue);
-	};
 
 	return (
-		<div className={classes.root}>
-			<div className={classes.tabsContainer}>
-				<StyledTabs value={value} onChange={handleChange} aria-label='styled tabs example'>
-					<StyledTab label='Home' />
-					<StyledTab label='I/UFO' />
-					<StyledTab label='Auction' />
-					<StyledTab label='Join us' />
-				</StyledTabs>
-				<Typography className={classes.padding} />
-			</div>
-		</div>
+		<Grid container justify='space-evenly' className={classes.tabsContainer}>
+			<Grid item className={classes.tab}>
+				<NavLink to='/' className={classes.link}>
+					Home
+				</NavLink>
+			</Grid>
+			<Grid item className={classes.tab}>
+				<NavLink to='/items' className={classes.link}>
+					I/UFO
+				</NavLink>
+			</Grid>
+			<Grid item className={classes.tab}>
+				Auction
+			</Grid>
+			<Grid item className={classes.tab}>
+				Join us
+			</Grid>
+		</Grid>
 	);
 };
 
