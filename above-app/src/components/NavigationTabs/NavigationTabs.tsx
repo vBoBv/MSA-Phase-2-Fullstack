7bfdaa37f1809d9682905f2433977a07778ac024
vBoBv <vannponhvath@gmail.com>
2020-09-11 13:42:19 +1200

    updated navigation tabs

--- above-app/src/components/NavigationTabs/NavigationTabs.tsx
+++ above-app/src/components/NavigationTabs/NavigationTabs.tsx
@@ -1,94 +1,77 @@
 import React from 'react';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import { makeStyles, withStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
-interface TabPanelProps {
-	children?: React.ReactNode;
-	dir?: string;
-	index: any;
-	value: any;
+interface StyledTabsProps {
+	value: number;
+	onChange: (event: React.ChangeEvent<{}>, newValue: number) => void;
 }
 
-function TabPanel(props: TabPanelProps) {
-	const { children, value, index, ...other } = props;
+const StyledTabs = withStyles({
+	indicator: {
+		display: 'flex',
+		justifyContent: 'center',
+		backgroundColor: 'transparent',
+		'& > span': {
+			maxWidth: 50,
+			width: '100%',
+			backgroundColor: '#26bdb8'
+		}
+	}
+})((props: StyledTabsProps) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
 
-	return (
-		<div
-			role='tabpanel'
-			hidden={value !== index}
-			id={`full-width-tabpanel-${index}`}
-			aria-labelledby={`full-width-tab-${index}`}
-			{...other}>
-			{value === index && (
-				<Box p={3}>
-					<Typography>{children}</Typography>
-				</Box>
-			)}
-		</div>
-	);
+interface StyledTabProps {
+	label: string;
 }
 
-function a11yProps(index: any) {
-	return {
-		id: `full-width-tab-${index}`,
-		'aria-controls': `full-width-tabpanel-${index}`
-	};
-}
+const StyledTab = withStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			textTransform: 'none',
+			color: '#fff',
+			fontWeight: theme.typography.fontWeightRegular,
+			fontSize: theme.typography.pxToRem(15),
+			marginRight: theme.spacing(1),
+			'&:focus': {
+				opacity: 1
+			}
+		}
+	})
+)((props: StyledTabProps) => <Tab disableRipple {...props} />);
 
 const useStyles = makeStyles((theme: Theme) => ({
 	root: {
-		backgroundColor: theme.palette.background.paper,
-		width: 500
+		flexGrow: 1
+	},
+	padding: {
+		paddingBottom: theme.spacing(3)
+	},
+	tabsContainer: {
+		backgroundColor: 'transparent'
 	}
 }));
 
-export default function NavigationTabs() {
+export default function CustomizedTabs() {
 	const classes = useStyles();
-	const theme = useTheme();
 	const [value, setValue] = React.useState(0);
 
 	const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
 		setValue(newValue);
 	};
 
-	const handleChangeIndex = (index: number) => {
-		setValue(index);
-	};
-
 	return (
 		<div className={classes.root}>
-			<AppBar position='static' color='default'>
-				<Tabs
-					value={value}
-					onChange={handleChange}
-					indicatorColor='primary'
-					textColor='primary'
-					variant='fullWidth'
-					aria-label='full width tabs example'>
-					<Tab label='Item One' {...a11yProps(0)} />
-					<Tab label='Item Two' {...a11yProps(1)} />
-					<Tab label='Item Three' {...a11yProps(2)} />
-				</Tabs>
-			</AppBar>
-			<SwipeableViews
-				axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-				index={value}
-				onChangeIndex={handleChangeIndex}>
-				<TabPanel value={value} index={0} dir={theme.direction}>
-					Item One
-				</TabPanel>
-				<TabPanel value={value} index={1} dir={theme.direction}>
-					Item Two
-				</TabPanel>
-				<TabPanel value={value} index={2} dir={theme.direction}>
-					Item Three
-				</TabPanel>
-			</SwipeableViews>
+			<div className={classes.tabsContainer}>
+				<StyledTabs value={value} onChange={handleChange} aria-label='styled tabs example'>
+					<StyledTab label='Home' />
+					<StyledTab label='UFO' />
+					<StyledTab label='Auction' />
+					<StyledTab label='Join us' />
+				</StyledTabs>
+				<Typography className={classes.padding} />
+			</div>
 		</div>
 	);
 }
