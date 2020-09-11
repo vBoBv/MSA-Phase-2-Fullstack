a00e1c666122f286c6fb7aeec37d51e2d08f1ed3
vBoBv <vannponhvath@gmail.com>
2020-09-11 13:14:04 +1200

    updated header

--- above-app/src/components/Header/Header.tsx
+++ above-app/src/components/Header/Header.tsx
@@ -17,6 +17,11 @@ const useStyles = makeStyles((theme) => ({
 		paddingLeft: '2rem',
 		zIndex: 1
 	},
+	logoContainer: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignContent: 'center'
+	},
 	logo: {
 		width: '50px',
 		height: '50px'
@@ -36,19 +41,22 @@ const Header = () => {
 	const classes = useStyles();
 
 	return (
-		<Grid container justify='flex-start' alignItems='center' className={classes.navBarContainer}>
-			<Grid item className={classes.logoItem}>
-				<img
-					className={`${classes.logo} wow rollIn rotatingAnimation`}
-					src={logo}
-					alt='Logo'
-					data-wow-duration='2s'
-					data-wow-delay='1.5s'
-				/>
+		<Grid container justify='space-between' alignItems='center' className={classes.navBarContainer}>
+			<Grid item className={classes.logoContainer}>
+				<Grid item>
+					<img
+						className={`${classes.logo} wow rollIn rotatingAnimation`}
+						src={logo}
+						alt='Logo'
+						data-wow-duration='2s'
+						data-wow-delay='1.5s'
+					/>
+				</Grid>
+				<Grid item className={classes.logoItem}>
+					<Typed strings={['Above']} typeSpeed={150} backSpeed={100} backDelay={10} smartBackspace />
+				</Grid>
 			</Grid>
-			<Grid item className={classes.logoItem}>
-				<Typed strings={['Above']} typeSpeed={150} backSpeed={100} backDelay={10} smartBackspace />
-			</Grid>
+
 			<Grid item>
 				<NavigationTabs />
 			</Grid>
