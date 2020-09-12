c9e49c0b5d39af9a988c59703eb27ededda4e2f4
vBoBv <vannponhvath@gmail.com>
2020-09-13 05:43:00 +1200

    refactored routes and replace browserrouter with router

--- above-app/src/components/App.tsx
+++ above-app/src/components/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from './Header/Header';
 import LandingPage from './LandingPage/LandingPage';
 import AcquiredObjects from './AcquiredObjects/AcquiredObjects';
-import SpaceObjects from './SpaceObjects/SpaceObjects';
+import SpaceObjectList from './SpaceObjects/SpaceObjectList';
 import SpaceObjectCreate from './SpaceObjects/SpaceObjectCreate';
+import SpaceObjectEdit from './SpaceObjects/SpaceObjectEdit';
+// import SpaceObjectDelete from './SpaceObjects/SpaceObjectDelete';
+import SimpleModal from './SpaceObjects/SpaceObjectDelete';
+
+import SpaceObjectShow from './SpaceObjects/SpaceObjectShow';
+import history from './history';
 
 import './App.css';
 
@@ -13,15 +19,25 @@ const App = () => {
 	return (
 		<div>
 			<CssBaseline />
-			<BrowserRouter>
+			<Router history={history}>
 				<Header />
 				<Switch>
 					<Route exact path='/' component={LandingPage} />
 					<Route exact path='/acquiredobjects' component={AcquiredObjects} />
-					<Route exact path='/spaceobjects' component={SpaceObjects} />
-					<Route exact path='/spaceobjects/create' component={SpaceObjectCreate} />
+					{/*  */}
+					{/* yes<Route exact path='/spaceobjects' component={SpaceObjects} /> */}
+					{/* <Route exact path='/spaceobjects/create' component={SpaceObjectCreate} /> */}
+					{/* yes<Route exact path={['/spaceobjects/create', '/spaceobjects/edit/:id']} component={SpaceObjectCreate} /> */}
+
+					{/* yes<Route exact path='/spaceobjects/:id' component={SpaceObject} /> */}
+					{/*  */}
+					<Route exact path='/spaceobjects' component={SpaceObjectList} />
+					<Route exact path='/spaceobjects/new' component={SpaceObjectCreate} />
+					<Route exact path='/spaceobjects/edit/:id' component={SpaceObjectEdit} />
+					<Route exact path='/spaceobjects/delete/:id' component={SimpleModal} />
+					<Route exact path='/spaceobjects/:id' component={SpaceObjectShow} />
 				</Switch>
-			</BrowserRouter>
+			</Router>
 		</div>
 	);
 };
