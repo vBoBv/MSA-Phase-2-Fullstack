9aaf192f988dea7d78ef1126c0293919cb7735dd
vBoBv <vannponhvath@gmail.com>
2020-09-16 03:02:22 +1200

    added language detector package for i18next

--- above-app/src/components/Translation/i18n.ts
+++ above-app/src/components/Translation/i18n.ts
@@ -1,9 +1,9 @@
 import i18n from 'i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-	// .use(LanguageDetector)
+	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
 		// we init with resources
@@ -45,7 +45,7 @@ i18n
 				}
 			}
 		},
-		fallbackLng: 'en',
+		fallbackLng: 'english',
 		debug: true,
 
 		// have a common namespace used around the full app
