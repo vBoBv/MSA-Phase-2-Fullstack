d69a85d8cac0696cf504a02958ad00d9501e0584
vBoBv <vannponhvath@gmail.com>
2020-09-16 17:48:00 +1200

    removed Modal and SpaceObjectDelete

--- above-app/src/components/SpaceObjects/SpaceObjectDelete.tsx
+++ above-app/src/components/SpaceObjects/SpaceObjectDelete.tsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import SpaceObjectForm from './SpaceObjectForm';
-import { IItem } from '../../common/Interfaces';
-import Api from '../api/Api';
-import { Typography } from '@material-ui/core';
-import { RouteComponentProps } from 'react-router-dom';
-import Modal from '../Modal/Modal';
+// import React, { useState, useEffect } from 'react';
+// import SpaceObjectForm from './SpaceObjectForm';
+// import { IItem } from '../../common/Interfaces';
+// import Api from '../api/Api';
+// import { Typography } from '@material-ui/core';
+// import { RouteComponentProps } from 'react-router-dom';
+// import Modal from '../Modal/Modal';
 
-interface IRouteComponentMatchParamProps {
-	id: string;
-}
+// interface IRouteComponentMatchParamProps {
+// 	id: string;
+// }
 
-const SpaceObjectDelete: React.FC<RouteComponentProps<IRouteComponentMatchParamProps>> = ({ match }) => {
-	const [spaceObject, setSpaceObject] = useState<IItem>({
-		id: '',
-		name: '',
-		description: '',
-		possession: ''
-	});
+// const SpaceObjectDelete: React.FC<RouteComponentProps<IRouteComponentMatchParamProps>> = ({ match }) => {
+// 	const [spaceObject, setSpaceObject] = useState<IItem>({
+// 		id: '',
+// 		name: '',
+// 		description: '',
+// 		possession: ''
+// 	});
 
-	useEffect(() => {
-		Api.Items.details(match.params.id).then((response) => {
-			setSpaceObject(response);
-		});
-	}, [match.params.id]);
+// 	useEffect(() => {
+// 		Api.Items.details(match.params.id).then((response) => {
+// 			setSpaceObject(response);
+// 		});
+// 	}, [match.params.id]);
 
-	const onDeleteSpaceObject = (id: string) => {
-		Api.Items.delete(spaceObject.id);
-	};
+// 	const onDeleteSpaceObject = (id: string) => {
+// 		Api.Items.delete(spaceObject.id);
+// 	};
 
-	return <Modal title='Delete Space Object' name={spaceObject.name} />;
-};
+// 	return <Modal title='Delete Space Object' name={spaceObject.name} />;
+// };
 
-export default SpaceObjectDelete;
+// export default SpaceObjectDelete;
