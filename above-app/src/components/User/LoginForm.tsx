4ae953f7c54da45cadfd2d1087ffa7339fdb0496
vBoBv <vannponhvath@gmail.com>
2020-09-16 17:45:29 +1200

    tweaked SignLoginForm and SignUpForm UI

--- above-app/src/components/User/LoginForm.tsx
+++ above-app/src/components/User/LoginForm.tsx
@@ -4,12 +4,30 @@ import Api from '../api/Api';
 import history from '../history';
 import { IUser } from '../../common/Interfaces';
 import { Trans } from 'react-i18next';
+import { Grid, Button } from '@material-ui/core';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 interface ILoginFormProps {
 	setUser: (user: IUser) => void;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		label: {
+			paddingRight: '1rem'
+		},
+		field: {
+			marginBottom: '1rem'
+		},
+		button: {
+			color: 'inherit'
+		}
+	})
+);
+
 const LoginForm: React.FC<ILoginFormProps> = ({ setUser }) => {
+	const classes = useStyles();
+
 	const [formValue, setFormValue] = useState<IUserForm>({
 		email: '',
 		password: ''
@@ -29,29 +47,39 @@ const LoginForm: React.FC<ILoginFormProps> = ({ setUser }) => {
 	};
 
 	return (
-		<div style={{ paddingTop: '10rem' }}>
-			<form className='ui form' onSubmit={(e) => onSubmit(e)}>
-				<div className='field'>
-					<label>
-						<Trans>Email</Trans>
-					</label>
-					<input type='text' name='email' placeholder='Email' onChange={onInputChange} value={formValue.email} />
-				</div>
-				<div className='field'>
-					<label>
-						<Trans>Password</Trans>
-					</label>
-					<input
-						name='password'
-						placeholder='Password'
-						value={formValue.password}
-						onChange={onInputChange}
-						type='password'
-					/>
-				</div>
-				<button className='ui button' type='submit'>
-					<Trans>Login</Trans>
-				</button>
+		<div>
+			<form onSubmit={(e) => onSubmit(e)}>
+				<Grid container justify='space-between' className={classes.field}>
+					<Grid item className={classes.label}>
+						<label htmlFor='email'>
+							<Trans>Email:</Trans>
+						</label>
+					</Grid>
+					<Grid item>
+						<input type='text' name='email' placeholder='Email' onChange={onInputChange} value={formValue.email} />
+					</Grid>
+				</Grid>
+				<Grid container justify='space-between' className={classes.field}>
+					<Grid item className={classes.label}>
+						<label>
+							<Trans>Password:</Trans>
+						</label>
+					</Grid>
+					<Grid item>
+						<input
+							name='password'
+							placeholder='Password'
+							value={formValue.password}
+							onChange={onInputChange}
+							type='password'
+						/>
+					</Grid>
+				</Grid>
+				<Grid container justify='center'>
+					<Button type='submit' className={classes.button} variant='outlined' color='primary'>
+						<Trans>Login</Trans>
+					</Button>
+				</Grid>
 			</form>
 		</div>
 	);
--- above-app/src/components/User/SignUpForm.tsx
+++ above-app/src/components/User/SignUpForm.tsx
@@ -4,12 +4,30 @@ import Api from '../api/Api';
 import history from '../history';
 import { IUser } from '../../common/Interfaces';
 import { Trans } from 'react-i18next';
+import { Grid, Button } from '@material-ui/core';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 interface ISignUpFormProps {
 	setUser: (user: IUser) => void;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		label: {
+			paddingRight: '1rem'
+		},
+		field: {
+			marginBottom: '1rem'
+		},
+		button: {
+			color: 'inherit'
+		}
+	})
+);
+
 const SignUpForm: React.FC<ISignUpFormProps> = ({ setUser }) => {
+	const classes = useStyles();
+
 	const [formValue, setFormValue] = useState<IUserForm>({
 		email: '',
 		password: ''
@@ -29,47 +47,71 @@ const SignUpForm: React.FC<ISignUpFormProps> = ({ setUser }) => {
 	};
 
 	return (
-		<div style={{ paddingTop: '10rem' }}>
-			<form className='ui form' onSubmit={(e) => onSubmit(e)}>
-				<div className='field'>
-					<label>
-						<Trans>Username</Trans>
-					</label>
-					<input
-						type='text'
-						name='username'
-						placeholder='username'
-						onChange={onInputChange}
-						value={formValue.username}
-					/>
-				</div>
-				<div className='field'>
-					<label>
-						<Trans>Name</Trans>
-					</label>
-					<input type='text' name='name' placeholder='name' onChange={onInputChange} value={formValue.name} />
-				</div>
-				<div className='field'>
-					<label>
-						<Trans>Email</Trans>
-					</label>
-					<input type='text' name='email' placeholder='Email' onChange={onInputChange} value={formValue.email} />
-				</div>
-				<div className='field'>
-					<label>
-						<Trans>Password</Trans>
-					</label>
-					<input
-						name='password'
-						placeholder='Password'
-						value={formValue.password}
-						onChange={onInputChange}
-						type='password'
-					/>
-				</div>
-				<button className='ui button' type='submit'>
-					<Trans>Sign up</Trans>
-				</button>
+		<div>
+			<form onSubmit={(e) => onSubmit(e)}>
+				<Grid container justify='space-between' className={classes.field}>
+					<Grid item className={classes.label}>
+						<label htmlFor='username'>
+							<Trans>Username:</Trans>
+						</label>
+					</Grid>
+					<Grid>
+						<input
+							type='text'
+							name='username'
+							placeholder='Username'
+							value={formValue.username || ''}
+							onChange={onInputChange}
+						/>
+					</Grid>
+				</Grid>
+				<Grid container justify='space-between' className={classes.field}>
+					<Grid item className={classes.label}>
+						<label htmlFor='name'>
+							<Trans>Name:</Trans>
+						</label>
+					</Grid>
+					<Grid item>
+						<input type='text' name='name' placeholder='name' onChange={onInputChange} value={formValue.name || ''} />
+					</Grid>
+				</Grid>
+				<Grid container justify='space-between' className={classes.field}>
+					<Grid item className={classes.label}>
+						<label htmlFor='email'>
+							<Trans>Email:</Trans>
+						</label>
+					</Grid>
+					<Grid item>
+						<input
+							type='text'
+							name='email'
+							placeholder='Email'
+							onChange={onInputChange}
+							value={formValue.email || ''}
+						/>
+					</Grid>
+				</Grid>
+				<Grid container justify='space-between' className={classes.field}>
+					<Grid item className={classes.label}>
+						<label htmlFor='password'>
+							<Trans>Password</Trans>
+						</label>
+					</Grid>
+					<Grid>
+						<input
+							name='password'
+							placeholder='Password'
+							value={formValue.password || ''}
+							onChange={onInputChange}
+							type='password'
+						/>
+					</Grid>
+				</Grid>
+				<Grid container justify='center'>
+					<Button type='submit' className={classes.button} variant='outlined' color='primary'>
+						<Trans>Sign up</Trans>
+					</Button>
+				</Grid>
 			</form>
 		</div>
 	);
