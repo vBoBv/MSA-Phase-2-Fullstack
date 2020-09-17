a225f4b04835f5282c240fe9ca4275b4c3ee691b
vBoBv <vannponhvath@gmail.com>
2020-09-17 23:14:02 +1200

    added getRandomImage function

---
+++ above-app/src/components/utils/getRandomImage.ts
@@ -0,0 +1,14 @@
+import planet1 from '../../assets/planet1.png';
+import planet2 from '../../assets/planet2.png';
+import planet3 from '../../assets/planet3.png';
+import planet7 from '../../assets/planet7.png';
+
+const images = [planet1, planet2, planet3, planet7];
+
+const getRandomImage = () => {
+	const RANGE = images.length;
+	console.log(RANGE);
+	return images[Math.floor(Math.random() * RANGE)];
+};
+
+export default getRandomImage;
